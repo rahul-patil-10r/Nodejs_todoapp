eeacb63740281ed52feb41142bc317144667e72a
Rahul Patil <rahulpatil@Rahuls-MacBook-Air.local>
2024-07-30 18:54:47 +0530

    second commit

--- data/database.js
+++ data/database.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
-export const database = () => {
-  mongoose.connect("mongodb://localhost:27017", {
-    dbName: "backendapp",
+import dotenv from "dotenv"
+dotenv.config()
+
+const mongoURI = process.env.MONGO_URI;
+const dbName = process.env.MONGO_DB_NAME;
+export const database = () => {``
+  mongoose.connect(mongoURI, {
+    dbName: dbName,
+  
   })
   .then(() => console.log("database connected"));
 };
